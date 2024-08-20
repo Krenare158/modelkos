9d08176b38f2a916c4c63a75053bae78f19ca554
Krenare158 <122061859+Krenare158@users.noreply.github.com>
2024-08-21 04:57:46 +0930

    Create CallToAction.js

---
+++ src/pages/Models/CallToAction.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './CallToAction.scss';
+
+const CallToAction = () => {
+  return (
+    <div className="call-to-action">
+      <h1 className='want-to'>Want to be here?</h1>
+      <p>ModelKos is seeking for new talent, so join the largest modeling agency in Kosovo and show off your skills. Be a model!</p>
+    </div>
+  );
+};
+
+export default CallToAction;
