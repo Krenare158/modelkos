5b0f0f16708ecde1e6393982e4d67dd1f41ecbd1
Krenare158 <122061859+Krenare158@users.noreply.github.com>
2024-08-21 04:50:34 +0930

    Create Apply.js

---
+++ src/Components/pages/Apply/Apply.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import becomemodel from './becomemodel';
+import ModelApplicationForm from './ModelApplicationForm';
+
+
+const Apply = () => {
+  return (
+    <div>
+      <becomemodel/>
+      <ModelApplicationForm/>
+    </div>
+  )
+}
+
+export default Apply
