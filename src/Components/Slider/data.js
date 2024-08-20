1cb7d15e91ba801af782d51a80e5f07d8bab373f
Krenare158 <122061859+Krenare158@users.noreply.github.com>
2024-08-21 04:52:22 +0930

    Create data.js

---
+++ src/Components/Slider/data.js
@@ -0,0 +1,80 @@
+import { FaMapMarked } from 'react-icons/fa';
+import modelSection1 from '../../assets/images/modelsection1.webp';
+import modelSection2 from '../../assets/images/modelsection2.webp';
+import modelSection3 from '../../assets/images/modelsection3.webp';
+import modelSection4 from '../../assets/images/modelsection4.webp';
+import modelSection5 from '../../assets/images/modelsection5.webp';
+import modelSection6 from '../../assets/images/modelsection6.webp';
+import modelSection7 from '../../assets/images/modelsection7.webp';
+import modelSection8 from '../../assets/images/modelsection8.webp';
+
+export const slideImages = [
+  {
+    src: modelSection1,
+    name: 'Anila Canolli',
+    time: '150/hour',
+    location: [{ icon: <FaMapMarked />, place: 'Prishtin, Kosova' }],
+    watches: '290'
+  },
+  {
+    src: modelSection2,
+    name: 'Model Name 2',
+    time: '120/hour',
+    location: [{ icon: <FaMapMarked />, place: 'Location 2' }],
+    watches: '150'
+  },
+  {
+    src: modelSection3,
+    name: 'Model Name 3',
+    time: '100/hour',
+    location: [{ icon: <FaMapMarked />, place: 'Location 3' }],
+    watches: '300'
+  },
+  {
+    src: modelSection4,
+    name: 'Model Name 4',
+    time: '130/hour',
+    location: [{ icon: <FaMapMarked />, place: 'Location 4' }],
+    watches: '250'
+  },
+  {
+    src: modelSection5,
+    name: 'Model Name 5',
+    time: '140/hour',
+    location: [{ icon: <FaMapMarked />, place: 'Location 5' }],
+    watches: '200'
+  },
+  {
+    src: modelSection6,
+    name: 'Model Name 6',
+    time: '160/hour',
+    location: [{ icon: <FaMapMarked />, place: 'Location 6' }],
+    watches: '180'
+  },
+  {
+    src: modelSection7,
+    name: 'Model Name 7',
+    time: '110/hour',
+    location: [{ icon: <FaMapMarked />, place: 'Location 7' }],
+    watches: '220'
+  },
+  {
+    src: modelSection8,
+    name: 'Model Name 8',
+    time: '125/hour',
+    location: [{ icon: <FaMapMarked />, place: 'Location 8' }],
+    watches: '270'
+  }
+];
+
+
+export const locations = [
+  'All',
+  'Prishtina',
+  'Peja',
+  'Ferizaji',
+  'Mitrovica',
+  'Gjakova',
+  'Gjilani',
+  'Prizreni'
+];
