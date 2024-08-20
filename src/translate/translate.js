a361abc1f21e597b50809b03c6a8115286a2765f
Krenare158 <122061859+Krenare158@users.noreply.github.com>
2024-08-21 05:09:30 +0930

    Add files via upload

---
+++ src/translate/translate.js
@@ -0,0 +1,13 @@
+export const translate = {
+    'en-GB' : require('./en-Gb.json'),
+    'sq-AL' : require('./sq-AL.json'),
+    'de-DE' : require('./en-Gb.json'),
+    'fr-FR' : require('./en-Gb.json'),
+    'it-IT' : require('./en-Gb.json'),
+    'es-ES' : require('./en-Gb.json'),
+    'tr-TR' : require('./en-Gb.json'),
+    'pr-PR' : require('./en-Gb.json'),
+    'rs-RS' : require('./en-Gb.json'),
+    'jp-JP' : require('./en-Gb.json'),
+    'ch-CH' : require('./en-Gb.json')
+};
